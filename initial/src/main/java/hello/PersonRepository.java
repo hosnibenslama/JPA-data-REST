2afefe7ce0e2a258097d140c1b1fd80c633ec884
Hosni BEN SLAMA <hosniinbusiness@gmail.com>
2017-08-16 16:00:55 +0100

    initial commit

--- initial/src/main/java/hello/PersonRepository.java
+++ initial/src/main/java/hello/PersonRepository.java
@@ -1,4 +1,14 @@
 package hello;
 
-public class PersonRepository {
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
 }
